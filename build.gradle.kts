4296f347db885968f98cb0683af3f23da2c3a170
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2022-11-29 22:16:56 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath(kotlin("gradle-plugin", version = "1.7.21"))
+    }
+}
+
+plugins {
+    idea
+    kotlin("jvm") version "1.7.21"
+}
+
+allprojects {
+    group = "io.github.tomplum"
+    version = "1.0.0"
+
+    apply(plugin = "kotlin")
+
+    repositories {
+        mavenCentral()
+        maven("https://maven.pkg.github.com/tomplum/advent-of-code-libs") {
+            credentials {
+                username = "TomPlum"
+                password = System.getenv("GITHUB_PACKAGE_REGISTRY_KEY")
+            }
+        }
+    }
+
+    dependencies {
+        implementation(kotlin("stdlib-jdk8"))
+        implementation(kotlin("reflect"))
+        implementation("io.github.tomplum:advent-of-code-libs:2.3.0")
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+}
