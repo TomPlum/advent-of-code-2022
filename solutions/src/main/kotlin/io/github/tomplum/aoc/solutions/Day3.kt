f85fc6b8fa4727950f26fcff09da41eea6d59abe
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-03 10:15:15 +0000

    Day 3 | Added solution part one

---
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day3.kt
@@ -0,0 +1,15 @@
+package io.github.tomplum.aoc.solutions
+
+import io.github.tomplum.aoc.sorting.RucksackArranger
+import io.github.tomplum.libs.input.Day
+import io.github.tomplum.libs.input.InputReader
+import io.github.tomplum.libs.solutions.Solution
+
+class Day3 : Solution<Int, Int> {
+    private val rucksacks = InputReader.read<String>(Day(3)).value
+    private val arranger = RucksackArranger(rucksacks)
+
+    override fun part1(): Int? {
+        return arranger.getDuplicateItemPrioritySum()
+    }
+}
