11053c313096f2ba05e4218ceffc1e761410a7d4
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-04 11:07:38 +0000

    Day 4 | Encapsulated assignment creation code inside companion function

--- implementation/src/main/kotlin/io/github/tomplum/aoc/assignment/CleaningAssigner.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/assignment/CleaningAssigner.kt
@@ -4,11 +4,10 @@ import io.github.tomplum.aoc.assignment.strategy.RedundantAssignmentFinderStrate
 
 class CleaningAssigner(private val sectionAssignments: List<String>) {
     fun countRedundantAssignments(strategy: RedundantAssignmentFinderStrategy): Int = sectionAssignments.sumOf { section ->
-        val split = section.split(",")
-        val first = split[0].split("-").toList().map { it.toInt() }
-        val second = split[1].split("-").toList().map { it.toInt() }
-        val firstAssignment = SectionAssignment(first[0], first[1])
-        val secondAssignment = SectionAssignment(second[0], second[1])
-        strategy.findRedundantAssignments(Pair(firstAssignment, secondAssignment))
+        val values = section.split(",")
+        val firstAssignment = SectionAssignment.fromString(values[0])
+        val secondAssignment = SectionAssignment.fromString(values[1])
+        val assignmentPair = Pair(firstAssignment, secondAssignment)
+        strategy.findRedundantAssignments(assignmentPair)
     }
 }
--- implementation/src/main/kotlin/io/github/tomplum/aoc/assignment/SectionAssignment.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/assignment/SectionAssignment.kt
@@ -1,3 +1,10 @@
 package io.github.tomplum.aoc.assignment
 
-data class SectionAssignment(val startingId: Int, val endingId: Int)
+data class SectionAssignment(val startingId: Int, val endingId: Int) {
+    companion object {
+        fun fromString(range: String): SectionAssignment {
+            val values = range.split("-").toList().map { value -> value.toInt() }
+            return SectionAssignment(values[0], values[1])
+        }
+    }
+}
