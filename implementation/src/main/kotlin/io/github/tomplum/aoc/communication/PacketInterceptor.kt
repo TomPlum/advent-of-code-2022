59931585fcf71c6e2f610a4e678759619630a68e
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-06 08:45:34 +0000

    Day 6 | Proved example inputs for part two

--- implementation/src/main/kotlin/io/github/tomplum/aoc/communication/PacketInterceptor.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/communication/PacketInterceptor.kt
@@ -3,8 +3,14 @@ package io.github.tomplum.aoc.communication
 class PacketInterceptor(private val datastream: String) {
     fun findStartOfPacketMarker(): Int {
         val segments = datastream.toList().windowed(4)
-        segments.forEachIndexed { i, chars -> if (!chars.containsDuplicates()) return segments.indexOf(chars) + 4 }
+        segments.forEach { chars -> if (!chars.containsDuplicates()) return segments.indexOf(chars) + 4 }
         throw IllegalArgumentException("Datastream [$datastream] does not contain a start-of-packet marker")
+    }
+
+    fun findStartOfMessageMarker(): Int {
+        val segments = datastream.toList().windowed(14)
+        segments.forEach { chars -> if (!chars.containsDuplicates()) return segments.indexOf(chars) + 14 }
+        throw IllegalArgumentException("Datastream [$datastream] does not contain a start-of-message marker")
     }
 
     private fun List<Char>.containsDuplicates(): Boolean {
--- implementation/src/test/kotlin/io/github/tomplum/aoc/communication/PacketInterceptorTest.kt
+++ implementation/src/test/kotlin/io/github/tomplum/aoc/communication/PacketInterceptorTest.kt
@@ -3,41 +3,78 @@ package io.github.tomplum.aoc.communication
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import io.github.tomplum.aoc.input.TestInputReader
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class PacketInterceptorTest {
-    @Test
-    fun exampleOne() {
-        val dataStream = TestInputReader.read<String>("/day6/example1.txt").asSingleString()
-        val packetInterceptor = PacketInterceptor(dataStream)
-        assertThat(packetInterceptor.findStartOfPacketMarker()).isEqualTo(7)
+    @Nested
+    inner class PacketStart {
+        @Test
+        fun exampleOne() {
+            val dataStream = TestInputReader.read<String>("/day6/example1.txt").asSingleString()
+            val packetInterceptor = PacketInterceptor(dataStream)
+            assertThat(packetInterceptor.findStartOfPacketMarker()).isEqualTo(7)
+        }
+
+        @Test
+        fun exampleTwo() {
+            val dataStream = TestInputReader.read<String>("/day6/example2.txt").asSingleString()
+            val packetInterceptor = PacketInterceptor(dataStream)
+            assertThat(packetInterceptor.findStartOfPacketMarker()).isEqualTo(5)
+        }
+
+        @Test
+        fun exampleThree() {
+            val dataStream = TestInputReader.read<String>("/day6/example3.txt").asSingleString()
+            val packetInterceptor = PacketInterceptor(dataStream)
+            assertThat(packetInterceptor.findStartOfPacketMarker()).isEqualTo(6)
+        }
+
+        @Test
+        fun exampleFour() {
+            val dataStream = TestInputReader.read<String>("/day6/example4.txt").asSingleString()
+            val packetInterceptor = PacketInterceptor(dataStream)
+            assertThat(packetInterceptor.findStartOfPacketMarker()).isEqualTo(10)
+        }
+
+        @Test
+        fun exampleFive() {
+            val dataStream = TestInputReader.read<String>("/day6/example5.txt").asSingleString()
+            val packetInterceptor = PacketInterceptor(dataStream)
+            assertThat(packetInterceptor.findStartOfPacketMarker()).isEqualTo(11)
+        }
     }
 
-    @Test
-    fun exampleTwo() {
-        val dataStream = TestInputReader.read<String>("/day6/example2.txt").asSingleString()
-        val packetInterceptor = PacketInterceptor(dataStream)
-        assertThat(packetInterceptor.findStartOfPacketMarker()).isEqualTo(5)
-    }
+    @Nested
+    inner class MessageStart {
+        @Test
+        fun exampleOne() {
+            val packetInterceptor = PacketInterceptor("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+            assertThat(packetInterceptor.findStartOfMessageMarker()).isEqualTo(19)
+        }
 
-    @Test
-    fun exampleThree() {
-        val dataStream = TestInputReader.read<String>("/day6/example3.txt").asSingleString()
-        val packetInterceptor = PacketInterceptor(dataStream)
-        assertThat(packetInterceptor.findStartOfPacketMarker()).isEqualTo(6)
-    }
+        @Test
+        fun exampleTwo() {
+            val packetInterceptor = PacketInterceptor("bvwbjplbgvbhsrlpgdmjqwftvncz")
+            assertThat(packetInterceptor.findStartOfMessageMarker()).isEqualTo(23)
+        }
 
-    @Test
-    fun exampleFour() {
-        val dataStream = TestInputReader.read<String>("/day6/example4.txt").asSingleString()
-        val packetInterceptor = PacketInterceptor(dataStream)
-        assertThat(packetInterceptor.findStartOfPacketMarker()).isEqualTo(10)
-    }
+        @Test
+        fun exampleThree() {
+            val packetInterceptor = PacketInterceptor("nppdvjthqldpwncqszvftbrmjlhg")
+            assertThat(packetInterceptor.findStartOfMessageMarker()).isEqualTo(23)
+        }
 
-    @Test
-    fun exampleFive() {
-        val dataStream = TestInputReader.read<String>("/day6/example5.txt").asSingleString()
-        val packetInterceptor = PacketInterceptor(dataStream)
-        assertThat(packetInterceptor.findStartOfPacketMarker()).isEqualTo(11)
+        @Test
+        fun exampleFour() {
+            val packetInterceptor = PacketInterceptor("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+            assertThat(packetInterceptor.findStartOfMessageMarker()).isEqualTo(29)
+        }
+
+        @Test
+        fun exampleFive() {
+            val packetInterceptor = PacketInterceptor("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+            assertThat(packetInterceptor.findStartOfMessageMarker()).isEqualTo(26)
+        }
     }
 }
