a37f0d84de6d22f44b2fc9902b1e4bfd0ba9ab03
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-09 09:02:36 +0000

    Day 9 | Added solution part one

---
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day9.kt
@@ -0,0 +1,16 @@
+package io.github.tomplum.aoc.solutions
+
+import io.github.tomplum.aoc.simluator.rope.RopeBridgeSimulator
+import io.github.tomplum.libs.input.Day
+import io.github.tomplum.libs.input.InputReader
+import io.github.tomplum.libs.solutions.Solution
+
+class Day9 : Solution<Int, Int> {
+
+    private val directions = InputReader.read<String>(Day(9)).value
+    private val ropeBridgeSimulator = RopeBridgeSimulator(directions)
+
+    override fun part1(): Int {
+        return ropeBridgeSimulator.countUniquePositionsVisited()
+    }
+}
