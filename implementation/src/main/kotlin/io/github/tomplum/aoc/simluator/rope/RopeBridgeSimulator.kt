74c2d26393e8167ed70cdc31a71261475ac5d69c
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-09 18:26:39 +0000

    Day 9 | Removed logging to improve performance

--- implementation/src/main/kotlin/io/github/tomplum/aoc/simluator/rope/RopeBridgeSimulator.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/simluator/rope/RopeBridgeSimulator.kt
@@ -1,16 +1,11 @@
 package io.github.tomplum.aoc.simluator.rope
 
-import io.github.tomplum.libs.logging.AdventLogger
 import io.github.tomplum.libs.math.Direction
 
 class RopeBridgeSimulator(private val directions: List<String>) {
 
-
     fun countUniquePositionsVisited(knotQuantity: Int = 1): Int {
         val bridge = RopeBridge(knotQuantity)
-
-        AdventLogger.debug("== Initial State ==")
-        AdventLogger.debug(bridge)
 
         directions.map { value ->
             val parts = value.split(" ")
@@ -24,10 +19,8 @@ class RopeBridgeSimulator(private val directions: List<String>) {
             val units = parts[1].toInt()
             Pair(direction, units)
         }.forEach { (direction, units) ->
-            AdventLogger.debug("== ${direction.name} $units ==")
             repeat(units) {
                 bridge.move(direction)
-                AdventLogger.debug(bridge)
             }
         }
 
