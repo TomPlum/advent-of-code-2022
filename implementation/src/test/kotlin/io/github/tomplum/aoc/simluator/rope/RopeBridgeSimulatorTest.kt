64e3fbbcb287cc13c00078a74e31fb1555da8c83
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-09 21:36:40 +0000

    Day 9 | Added example one part two test case

--- implementation/src/test/kotlin/io/github/tomplum/aoc/simluator/rope/RopeBridgeSimulatorTest.kt
+++ implementation/src/test/kotlin/io/github/tomplum/aoc/simluator/rope/RopeBridgeSimulatorTest.kt
@@ -8,14 +8,21 @@ import org.junit.jupiter.api.Test
 class RopeBridgeSimulatorTest {
 
     @Test
-    fun examplePartOne() {
+    fun exampleOnePartOne() {
         val directions = TestInputReader.read<String>("day9/example.txt").value
         val simulator = RopeBridgeSimulator(directions)
         assertThat(simulator.countUniquePositionsVisited()).isEqualTo(13)
     }
 
     @Test
-    fun examplePartTwo() {
+    fun exampleOnePartTwo() {
+        val directions = TestInputReader.read<String>("day9/example.txt").value
+        val simulator = RopeBridgeSimulator(directions)
+        assertThat(simulator.countUniquePositionsVisited(9)).isEqualTo(1)
+    }
+
+    @Test
+    fun exampleTwoPartTwo() {
         val directions = TestInputReader.read<String>("day9/example-large.txt").value
         val simulator = RopeBridgeSimulator(directions)
         assertThat(simulator.countUniquePositionsVisited(9)).isEqualTo(36)
