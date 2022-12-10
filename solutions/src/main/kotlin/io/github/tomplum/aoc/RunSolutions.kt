d924d16eab247089ad44b1a6ffd510fff0354fb7
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-10 11:50:24 +0000

    Day 10 | Added solution part one

--- solutions/src/main/kotlin/io/github/tomplum/aoc/RunSolutions.kt
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/RunSolutions.kt
@@ -4,5 +4,5 @@ import io.github.tomplum.aoc.solutions.*
 import io.github.tomplum.libs.solutions.SolutionRunner
 
 fun main() {
-    SolutionRunner.run(Day1(), Day2(), Day3(), Day4(), Day5(), Day6(), Day7(), Day8(), Day9())
+    SolutionRunner.run(Day1(), Day2(), Day3(), Day4(), Day5(), Day6(), Day7(), Day8(), Day9(), Day10())
 }
---
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day10.kt
@@ -0,0 +1,16 @@
+package io.github.tomplum.aoc.solutions
+
+import io.github.tomplum.aoc.communication.cpu.ClockCircuit
+import io.github.tomplum.libs.input.Day
+import io.github.tomplum.libs.input.InputReader
+import io.github.tomplum.libs.solutions.Solution
+
+class Day10 : Solution<Int, Int> {
+
+    private val program = InputReader.read<String>(Day(10)).value
+    private val clockCircuit = ClockCircuit(program)
+
+    override fun part1(): Int {
+        return clockCircuit.run(listOf(20, 60, 100, 140, 180, 220))
+    }
+}
