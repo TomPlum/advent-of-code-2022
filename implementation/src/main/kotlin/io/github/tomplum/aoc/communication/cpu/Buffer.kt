c77dc9e3f84e9a9e58fc7e8619a287364394f973
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-10 12:42:26 +0000

    Day 10 | Fixed printing bug

--- implementation/src/main/kotlin/io/github/tomplum/aoc/communication/cpu/Buffer.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/communication/cpu/Buffer.kt
@@ -8,23 +8,16 @@ data class Buffer(val snapshots: List<RegisterSnapshot>) {
         snapshots[cycle - 1].xRegister * cycle
     }
 
-    fun print() {
-        val display = StringBuilder()
-
-        snapshots.drop(1).chunked(40).forEach { line ->
-            var spritePosition: Int
-            line.forEach { snapshot ->
-                val currentDrawingIndex = (snapshot.cycle % 40) - 1
-                spritePosition = snapshot.xRegister
-                val sprite = (spritePosition - 1)..(spritePosition + 1)
-                if (currentDrawingIndex in sprite) {
-                    display.append("#")
-                } else {
-                    display.append(".")
-                }
-            }
-            display.append("\n")
+    fun print() = snapshots.chunked(40).fold(StringBuilder()) { display, line ->
+        var spritePosition: Int
+        line.forEach { snapshot ->
+            val currentDrawingIndex = (snapshot.cycle % 40) - 1
+            spritePosition = snapshot.xRegister
+            val sprite = (spritePosition - 1)..(spritePosition + 1)
+            display.append(if (currentDrawingIndex in sprite) "#" else ".")
         }
-        AdventLogger.info(display)
-    }
+        display.append("\n")
+        display
+    }.also { display -> AdventLogger.info(display) }
+
 }
