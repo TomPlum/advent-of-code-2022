9dd4bdcd5fa62c9535293909d85b566834c16852
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-11 13:25:25 +0000

    Day 11 | Removed logging

--- implementation/src/main/kotlin/io/github/tomplum/aoc/simluator/monkey/KeepAwaySimulator.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/simluator/monkey/KeepAwaySimulator.kt
@@ -1,17 +1,15 @@
 package io.github.tomplum.aoc.simluator.monkey
 
 import io.github.tomplum.libs.extensions.product
-import io.github.tomplum.libs.logging.AdventLogger
 
 class KeepAwaySimulator(notes: List<String>) {
 
     private val monkeys = MonkeyNoteParser().parse(notes)
-
+    private val mod = monkeys.map { monkey -> monkey.test.divisor }.reduce { a, b -> a * b }
     private val inspections = mutableMapOf(*monkeys.map { Pair(it.id, 0L) }.toTypedArray())
 
     fun simulate(rounds: Int): Long {
-        val mod = monkeys.map { monkey -> monkey.test.divisor }.reduce { a, b -> a * b }
-        (1..rounds).forEach { round ->
+        repeat(rounds) {
             monkeys.forEach { monkey ->
                 monkey.items.forEach { item ->
                     val worryLevel = monkey.operation.execute(item) % mod
@@ -20,12 +18,6 @@ class KeepAwaySimulator(notes: List<String>) {
                     monkeys[targetMonkey].items.add(worryLevel)
                 }
                 monkey.items.clear()
-            }
-            if (round == 1 || round == 20 || round % 1000 == 0) {
-                AdventLogger.info("== After round $round")
-                inspections.forEach { (monkey, count) ->
-                    AdventLogger.info("Monkey $monkey inspected items $count times.")
-                }
             }
         }
 
