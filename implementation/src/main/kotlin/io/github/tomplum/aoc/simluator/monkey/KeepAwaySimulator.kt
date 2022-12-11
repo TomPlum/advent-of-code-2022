75ee7cd9082e49a28072bd215ccafc9454aa02bd
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-11 12:35:21 +0000

    Day 11 | Proved example part one

--- implementation/src/main/kotlin/io/github/tomplum/aoc/simluator/monkey/KeepAwaySimulator.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/simluator/monkey/KeepAwaySimulator.kt
@@ -1,11 +1,25 @@
 package io.github.tomplum.aoc.simluator.monkey
 
+import io.github.tomplum.libs.extensions.product
+
 class KeepAwaySimulator(notes: List<String>) {
 
     private val monkeys = MonkeyNoteParser().parse(notes)
 
+    private val inspections = mutableMapOf(*monkeys.map { Pair(it.id, 0) }.toTypedArray())
+
     fun simulate(rounds: Int): Int {
-        var worryLevel = 0
-        return 0
+        (1..rounds).forEach { round ->
+            monkeys.forEach { monkey ->
+                monkey.items.forEach { item ->
+                    val worryLevel = monkey.operation.execute(item) / 3
+                    inspections.computeIfPresent(monkey.id) { _, count  -> count +  1 }
+                    val targetMonkey = monkey.test.execute(worryLevel)
+                    monkeys[targetMonkey].items.add(worryLevel)
+                }
+                monkey.items.clear()
+            }
+        }
+        return inspections.values.sortedDescending().take(2).product()
     }
 }
--- implementation/src/main/kotlin/io/github/tomplum/aoc/simluator/monkey/Monkey.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/simluator/monkey/Monkey.kt
@@ -2,7 +2,7 @@ package io.github.tomplum.aoc.simluator.monkey
 
 data class Monkey(
     val id: Int,
-    val startingItems: List<Int>,
+    val items: MutableList<Int>,
     val operation: MonkeyOperation,
     val test: MonkeyTest
 )
