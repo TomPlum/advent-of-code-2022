18d63dc0a535e458b9f8b11c18d3f561fd6934df
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-12 10:02:25 +0000

    Day 12 | Added part one solution

--- solutions/src/main/kotlin/io/github/tomplum/aoc/RunSolutions.kt
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/RunSolutions.kt
@@ -6,6 +6,6 @@ import io.github.tomplum.libs.solutions.SolutionRunner
 fun main() {
     SolutionRunner.run(
         Day1(), Day2(), Day3(), Day4(), Day5(), Day6(), Day7(), Day8(), Day9(), Day10(),
-        Day11()
+        Day11(), Day12()
     )
 }
---
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day12.kt
@@ -0,0 +1,16 @@
+package io.github.tomplum.aoc.solutions
+
+import io.github.tomplum.aoc.map.hill.HillHeightMap
+import io.github.tomplum.libs.input.Day
+import io.github.tomplum.libs.input.InputReader
+import io.github.tomplum.libs.solutions.Solution
+
+class Day12 : Solution<Int, Int> {
+
+    private val data = InputReader.read<String>(Day(12)).value
+    private val hillHeightMap = HillHeightMap(data)
+
+    override fun part1(): Int {
+        return hillHeightMap.findShortestRouteToBestSignal()
+    }
+}
