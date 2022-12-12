abdc702521b0daf48bef805abcd979500cd7377a
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-12 18:33:10 +0000

    Day 12 | Improved performance, converted tile functions to properties

--- implementation/src/main/kotlin/io/github/tomplum/aoc/map/hill/HillHeightMap.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/map/hill/HillHeightMap.kt
@@ -6,6 +6,8 @@ import java.util.PriorityQueue
 
 class HillHeightMap(data: List<String>) : AdventMap2D<HillTile>() {
 
+    private val adjacencyMatrix = mutableMapOf<Point2D, Set<Point2D>>()
+
     init {
         var x = 0
         var y = 0
@@ -20,42 +22,50 @@ class HillHeightMap(data: List<String>) : AdventMap2D<HillTile>() {
     }
 
     fun findShortestRouteToBestSignal(): Int {
-        return searchForBestSignalFrom { tile -> tile.isCurrentPosition() }
+        return searchForBestSignalFrom { tile -> tile.isCurrentPosition }
     }
 
     fun findShortestRouteFromLowestElevationToBestSignal(): Int {
-        return searchForBestSignalFrom { tile -> tile.isLowestPossibleElevation() }
+        return searchForBestSignalFrom { tile -> tile.isLowestPossibleElevation }
     }
 
     private fun searchForBestSignalFrom(startingTileFilter: (tile: HillTile) -> Boolean): Int {
         val distances = mutableMapOf<Point2D, Int>()
-        val queue = PriorityQueue<Point2D>()
+        val next = PriorityQueue<Point2D>()
 
         filterTiles { tile -> startingTileFilter(tile) }.keys.forEach { position ->
-            queue.offer(position)
+            next.offer(position)
             distances[position] = 0
         }
 
-        while(queue.isNotEmpty()) {
-            val currentPos = queue.poll()
+        while(next.isNotEmpty()) {
+            val currentPos = next.poll()
             val distance = distances[currentPos]!!
             currentPos.traversableAdjacent().forEach { adjacentPos ->
                 val updatedDistance = distance + 1
                 if (updatedDistance < distances.getOrDefault(adjacentPos, Int.MAX_VALUE)) {
                     distances[adjacentPos] = updatedDistance
-                    queue.add(adjacentPos)
+                    next.add(adjacentPos)
                 }
             }
         }
 
-        val end = filterTiles { tile -> tile.isBestSignal() }.keys.first()
+        val end = filterTiles { tile -> tile.isBestSignal }.keys.first()
         return distances.filterKeys { pos -> pos == end }.values.min()
     }
 
     private fun Point2D.traversableAdjacent(): Set<Point2D> {
-        return this.orthogonallyAdjacent()
-            .filter { pos -> hasRecorded(pos) }
-            .filter { dest -> getTile(this).canClimbTo(getTile(dest)) }
-            .toSet()
+        val cached = adjacencyMatrix[this]
+
+        if (cached == null) {
+            val adjacent = this.orthogonallyAdjacent()
+                .filter { pos -> hasRecorded(pos) }
+                .filter { dest -> getTile(this).canClimbTo(getTile(dest)) }
+                .toSet()
+            adjacencyMatrix[this] = adjacent
+            return adjacent
+        }
+
+        return cached
     }
 }
--- implementation/src/main/kotlin/io/github/tomplum/aoc/map/hill/HillTile.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/map/hill/HillTile.kt
@@ -2,20 +2,19 @@ package io.github.tomplum.aoc.map.hill
 
 import io.github.tomplum.libs.math.map.MapTile
 
-class HillTile(private val marker: Char) : MapTile<Char>(marker) {
-    fun isCurrentPosition() = marker == 'S'
+class HillTile(marker: Char) : MapTile<Char>(marker) {
 
-    fun isBestSignal() = marker == 'E'
+    val isBestSignal = marker == 'E'
+    val isCurrentPosition = marker == 'S'
+    val isLowestPossibleElevation = marker == 'a' || isCurrentPosition
 
-    fun canClimbTo(dest: HillTile): Boolean {
-        return dest.height() - this.height() <= 1
+    private val height = when {
+        isCurrentPosition -> 'a'.code
+        isBestSignal -> 'z'.code
+        else -> marker.code
     }
 
-    fun isLowestPossibleElevation() = marker == 'a' || isCurrentPosition()
-
-    private fun height() = when {
-        isCurrentPosition() -> 'a'.code
-        isBestSignal() -> 'z'.code
-        else -> marker.code
+    fun canClimbTo(dest: HillTile): Boolean {
+        return dest.height - this.height <= 1
     }
 }
