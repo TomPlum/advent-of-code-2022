000774e0395c44a2a54ae0e64a1f8314224c8e3c
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-13 11:14:34 +0000

    Day 13 | Added solution part one

---
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day13.kt
@@ -0,0 +1,15 @@
+package io.github.tomplum.aoc.solutions
+
+import io.github.tomplum.aoc.communication.signal.DistressSignal
+import io.github.tomplum.libs.input.Day
+import io.github.tomplum.libs.input.InputReader
+import io.github.tomplum.libs.solutions.Solution
+
+class Day13 : Solution<Int, Int> {
+    private val data = InputReader.read<String>(Day(13)).value
+    private val distressSignal = DistressSignal(data)
+
+    override fun part1(): Int {
+        return distressSignal.determinePacketPairOrdering()
+    }
+}
