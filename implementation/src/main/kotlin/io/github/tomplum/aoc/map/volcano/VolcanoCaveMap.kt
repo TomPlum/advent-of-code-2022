f21fd8161822bb649c424136f8c558af7c3b9e18
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-18 18:17:54 +0000

    Day 16 | Started part one

--- implementation/src/main/kotlin/io/github/tomplum/aoc/map/volcano/VolcanoCaveMap.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/map/volcano/VolcanoCaveMap.kt
@@ -1,25 +1,26 @@
 package io.github.tomplum.aoc.map.volcano
 
+import io.github.tomplum.libs.extensions.cartesianProduct
 import java.util.*
 
 class VolcanoCaveMap(scan: List<String>) {
-    private val flowRates = mutableMapOf<String, Int>()
+    private val flowRates = mutableMapOf<Char, Int>()
 
-    private val valves: Map<String, List<String>> = scan.associate { line ->
-        val label = line.removePrefix("Valve ").split(" ")[0].trim()
+    private val valves: Map<Char, List<Char>> = scan.associate { line ->
+        val label = line.removePrefix("Valve ").split(" ")[0].trim().first()
         val flowRate = line.split(" has flow rate=")[1].split(";")[0].toInt()
         val tunnels = if (line.contains("tunnels")) {
-            line.split("tunnels lead to valves ")[1].trim().split(", ")
+            line.split("tunnels lead to valves ")[1].trim().split(", ").map { it.first() }
         } else {
-            listOf(line.split("tunnel leads to valve ")[1].trim())
+            listOf(line.split("tunnel leads to valve ")[1].trim()).map { it.first() }
         }
         flowRates[label] = flowRate
         label to tunnels
     }
 
-    fun findMaximumFlowRate() {
-        val distances = mutableMapOf<String, Int>()
-        val next = PriorityQueue<String>()
+    fun findMaximumFlowRate(): Int {
+        /*val distances = mutableMapOf<Char, Int>()
+        val next = PriorityQueue<Char>()
 
         val start = valves.keys.first()
         next.offer(start)
@@ -35,15 +36,90 @@ class VolcanoCaveMap(scan: List<String>) {
                     next.add(adjacentValve)
                 }
             }
+        }*/
+
+        val valveLabels = valves.keys.toList()
+        val distinctCombinations = valveLabels.cartesianProduct(valveLabels).filter { (a, b) -> a != b }
+        val paths = Array(26) { Array(26) { "" } }
+        valveLabels.forEach { start ->
+            valveLabels.forEach { end ->
+                if (start != end) {
+                    paths[start.getAlphabetIndex()][end.getAlphabetIndex()] = findShortestPath(start, end)
+                }
+            }
         }
 
-        val valveCandidates = valves.filterKeys { label -> flowRates[label]!! > 0 }
+        val valveCandidates = valveLabels.filter { label -> flowRates[label]!! > 0 }
 
-        val s = ""
+        val rates = calculateFlowRates(paths, 'A', 30, valveCandidates)
+        return rates.map { rates ->
+            rates.entries.fold(0) { pressure, (valve, time) -> pressure + flowRates[valve]!! * time }
+        }.max()
+
+        /*rates.map { rates ->
+            rates.entries.sumOf { (valve, time) -> flowRates[valve]!! * time }
+        }.maxOf { pressure -> pressure }*/
     }
 
-    private fun find() {
+    private fun calculateFlowRates(
+        paths: Array<Array<String>>,
+        source: Char,
+        time: Int,
+        remaining: List<Char>,
+        opened: Map<Char, Int> = mutableMapOf()
+    ): List<Map<Char, Int>> {
+        // A list of valve label -> time that valve spends open
+        val flowRates = mutableListOf(opened)
 
+        remaining.forEachIndexed { i, target ->
+            val path = paths[source.getAlphabetIndex()][target.getAlphabetIndex()]
+            val newTime = time - (path.length - 1)
+            if (newTime < 1) {
+                return@forEachIndexed
+            }
+
+            val newlyOpened = opened.toMutableMap()
+            newlyOpened[target] = newTime
+
+            val newRemaining = remaining.toMutableList()
+            newRemaining.removeAt(i)
+
+            flowRates.addAll(calculateFlowRates(paths, target, newTime, newRemaining, newlyOpened))
+        }
+
+        return flowRates
+    }
+
+    private fun findShortestPath(startingValve: Char, finishingValve: Char): String {
+        val visited = mutableSetOf(startingValve)
+
+        val next = PriorityQueue<String>()
+        next.offer(startingValve.toString())
+
+        while(next.isNotEmpty()) {
+            val path = next.poll()
+            val valve = path.last()
+
+            valves[valve]?.forEach { adjacent ->
+                if (adjacent in visited) {
+                    return@forEach
+                }
+
+                val updatedPath = "$path$adjacent"
+                if (adjacent == finishingValve) {
+                    return updatedPath
+                }
+
+                visited.add(adjacent)
+                next.add(updatedPath)
+            }
+        }
+
+        throw IllegalArgumentException("Could not find path from $startingValve -> $finishingValve")
+    }
+
+    private fun Char.getAlphabetIndex(): Int {
+        return this.lowercase().first().code - 'a'.code
     }
 
 }
