b78bd252f0a15ee97491eef61500beb86d45fda5
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-18 10:16:42 +0000

    Day 18 | Added solution part one

---
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day18.kt
@@ -0,0 +1,15 @@
+package io.github.tomplum.aoc.solutions
+
+import io.github.tomplum.aoc.map.pond.PondMap
+import io.github.tomplum.libs.input.Day
+import io.github.tomplum.libs.input.InputReader
+import io.github.tomplum.libs.solutions.Solution
+
+class Day18 : Solution<Int, Int> {
+    private val scan = InputReader.read<String>(Day(18)).value
+    private val pondMap = PondMap(scan)
+
+    override fun part1(): Int {
+        return pondMap.getLavaDropletSurfaceArea()
+    }
+}
