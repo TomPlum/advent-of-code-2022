87a373e5c115a1dee38190853c5e57ac52c73b1d
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-19 17:47:44 +0000

    Day 19 | Added missing ore collecting robot creation

--- implementation/src/main/kotlin/io/github/tomplum/aoc/simluator/robot/Blueprint.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/simluator/robot/Blueprint.kt
@@ -11,20 +11,6 @@ data class Blueprint(
         fun fromString(value: String): Blueprint {
             val parts = value.trim().split(" ")
 
-/*            val id = parts[0].removePrefix("Blueprint ").removeSuffix(":").toInt()
-            val oreRobotCost = parts[1].removePrefix("Each ore robot costs ").removeSuffix(" ore.").toInt()
-            val clayRobotCost = parts[1].removePrefix("Each clay robot costs ").removeSuffix(" ore.").toInt()
-
-            val obsidianRobotParts = parts[2].split(" and ")
-            val obsidianRobotOreCost = obsidianRobotParts[0].removePrefix("Each obsidian robot costs ").removeSuffix(" ore").toInt()
-            val obsidianRobotClayCost = obsidianRobotParts[1].removeSuffix(" clay.").toInt()
-            val obsidianRobotCost = Pair(obsidianRobotOreCost, obsidianRobotClayCost)
-
-            val geodeRobotParts = parts[2].split(" and ")
-            val geodeRobotOreCost = geodeRobotParts[0].removePrefix("Each geode robot costs ").removeSuffix(" ore").toInt()
-            val geodeRobotObsidianCost = geodeRobotParts[1].removeSuffix(" obsidian.").toInt()
-            val geodeRobotCost = Pair(geodeRobotOreCost, geodeRobotObsidianCost)*/
-
             val id = parts[1].removeSuffix(":").toInt()
             val oreRobotCost = parts[6].toInt()
             val clayRobotCost = parts[12].toInt()
@@ -34,6 +20,10 @@ data class Blueprint(
             return Blueprint(id, oreRobotCost, clayRobotCost, obsidianRobotCost, geodeRobotCost)
         }
     }
+
+    val maxOreCost = listOf(oreRobotCost, clayRobotCost, obsidianRobotCost.first, geodeRobotCost.first).max()
+    val maxClayCost = obsidianRobotCost.second
+    val maxObsidianCost = geodeRobotCost.second
 
     fun canAffordObsidianRobot(ore: Int, clay: Int): Boolean {
         return ore >= obsidianRobotCost.first && clay >= obsidianRobotCost.second
@@ -45,5 +35,9 @@ data class Blueprint(
 
     fun canAffordClayRobot(ore: Int): Boolean {
         return ore >= clayRobotCost
+    }
+
+    fun canAffordOreRobot(ore: Int): Boolean {
+        return ore >= oreRobotCost
     }
 }
--- implementation/src/main/kotlin/io/github/tomplum/aoc/simluator/robot/OreCollectingRobotSimulator.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/simluator/robot/OreCollectingRobotSimulator.kt
@@ -1,41 +1,65 @@
 package io.github.tomplum.aoc.simluator.robot
 
+import io.github.tomplum.libs.extensions.nthBinomialCoefficient
+
 class OreCollectingRobotSimulator(data: List<String>) {
 
     private val blueprints = data.map { info -> Blueprint.fromString(info) }
 
     private val cache = mutableMapOf<String, List<Int>>()
+    private var maxGeodesFound = 0
 
+    /**
+     * 549 is too low for the solution
+     */
     fun simulate(): Int {
         val start = InventoryState(0, 1, 0, 0, 0, 0, 0, 0)
-        return blueprints.asSequence().map { blueprint ->
+        val geodes = blueprints.map { blueprint ->
             blueprint.id to calculateGeodesSmashed(blueprint, 1, start).max()
-        }.sumOf { (id, geodes) -> id * geodes }
+        }
+        return geodes.sumOf { (id, geodes) -> id * geodes }
     }
 
     private fun calculateGeodesSmashed(blueprint: Blueprint, minute: Int, inventory: InventoryState): List<Int> {
         if (minute <= 24) {
             val states = mutableListOf<InventoryState>()
 
+            val timeRemaining = 24 - minute
+            val optimisticGeodePrediction = (timeRemaining - 1).nthBinomialCoefficient()
+            if (optimisticGeodePrediction <= maxGeodesFound) {
+                return listOf(inventory.openGeodes)
+            }
+
             if (inventory.canAffordGeodeCrackingRobot(blueprint)) {
                 states += inventory.createGeodeCrackingRobot(
                     blueprint.geodeRobotCost.first,
                     blueprint.geodeRobotCost.second
                 )
-            }
+            } else {
+                val hasEnoughObsidian = inventory.producesEnoughObsidianToMakeGeodeRobots(blueprint)
+                if (!hasEnoughObsidian && inventory.canAffordObsidianRobot(blueprint)) {
+                    states += inventory.createObsidianCollectingRobot(
+                        blueprint.obsidianRobotCost.first,
+                        blueprint.obsidianRobotCost.second
+                    )
+                }
 
-            if (inventory.canAffordObsidianRobot(blueprint)) {
-                states += inventory.createObsidianCollectingRobot(
-                    blueprint.obsidianRobotCost.first,
-                    blueprint.obsidianRobotCost.second
-                )
-            }
+                val hasEnoughClay = inventory.producesEnoughClayToMakeObsidianRobots(blueprint)
+                if (!hasEnoughClay && inventory.canAffordClayRobot(blueprint)) {
+                    states += inventory.createClayCollectionRobot(blueprint.clayRobotCost)
+                }
 
-            if (inventory.canAffordClayRobot(blueprint)) {
-                states += inventory.createClayCollectionRobot(blueprint.clayRobotCost)
-            }
+                val hasEnoughOre = inventory.producesEnoughOreToMakingEverything(blueprint)
+                if (!hasEnoughOre && inventory.canAffordOreRobot(blueprint)) {
+                    states += inventory.createOreCollectionRobot(blueprint.oreRobotCost)
+                }
 
-            states += inventory.createNothing()
+                if (!hasEnoughOre || !hasEnoughClay || !hasEnoughObsidian) {
+                    states += inventory.createNothing()
+                } else {
+                    states += inventory
+                }
+            }
 
             return states.flatMap { nextState ->
                 cache.getOrPut("${blueprint.id}${nextState.key()}") {
@@ -43,7 +67,9 @@ class OreCollectingRobotSimulator(data: List<String>) {
                 }
             }
         } else {
-            return listOf(inventory.openGeodes)
+            val geodes = inventory.openGeodes
+            if (geodes > maxGeodesFound) maxGeodesFound = geodes
+            return listOf(geodes)
         }
     }
 
@@ -92,10 +118,33 @@ class OreCollectingRobotSimulator(data: List<String>) {
             return state
         }
 
+        fun createOreCollectionRobot(oreCost: Int): InventoryState {
+            val state = this.copy()
+            state.ore = ore - oreCost
+
+            state.collect()
+
+            state.oreCollectingRobots += 1
+
+            return state
+        }
+
         fun createNothing(): InventoryState {
             val state = this.copy()
             state.collect()
             return state
+        }
+
+        fun producesEnoughOreToMakingEverything(blueprint: Blueprint): Boolean {
+            return oreCollectingRobots >= blueprint.maxOreCost
+        }
+
+        fun producesEnoughClayToMakeObsidianRobots(blueprint: Blueprint): Boolean {
+            return clayCollectingRobots >= blueprint.maxClayCost
+        }
+
+        fun producesEnoughObsidianToMakeGeodeRobots(blueprint: Blueprint): Boolean {
+            return obsidianCollectingRobots >= blueprint.maxObsidianCost
         }
 
         private fun collect() {
@@ -115,6 +164,10 @@ class OreCollectingRobotSimulator(data: List<String>) {
 
         fun canAffordClayRobot(blueprint: Blueprint): Boolean {
             return blueprint.canAffordClayRobot(ore)
+        }
+
+        fun canAffordOreRobot(blueprint: Blueprint): Boolean {
+            return blueprint.canAffordOreRobot(ore)
         }
 
         fun key(): String {
