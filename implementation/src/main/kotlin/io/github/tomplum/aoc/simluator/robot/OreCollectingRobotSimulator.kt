71f3ac2730cb2abbad2949c11fe47264fd66f156
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-19 11:49:55 +0000

    Day 19 | Initial recursive implementation

--- implementation/src/main/kotlin/io/github/tomplum/aoc/simluator/robot/OreCollectingRobotSimulator.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/simluator/robot/OreCollectingRobotSimulator.kt
@@ -6,97 +6,185 @@ class OreCollectingRobotSimulator(data: List<String>) {
 
     private val blueprints = data.map { info -> Blueprint.fromString(info) }
 
+    private val cache = mutableMapOf<String, List<Pair<Int, Int>>>()
+
     fun simulate(): Int {
-        val result = blueprints.map { blueprint ->
-            var minute = 1
+        val start = CollectionState(0, 1, 0, 0, 0, 0, 0, 0)
+        return blueprints.asSequence().map { blueprint -> recurse(blueprint, 1, start)
+            .maxBy { (_, geodes) -> geodes } }
+            .sumOf { (id, geodes) -> id * geodes }
+    }
 
-            var ore = 0
-            var oreCollectingRobots = 1
+    private fun recurse(blueprint: Blueprint, minute: Int, state: CollectionState): List<Pair<Int, Int>> {
+        if (minute <= 24) {
+            //AdventLogger.debug("== Minute $minute ==")
 
-            var clay = 0
-            var clayCollectingRobots = 0
+            val decisionStates = mutableListOf<CollectionState>()
 
-            var obsidian = 0
-            var obsidianCollectingRobots = 0
+            // Resources are spent immediately on the factory when available
+            if (blueprint.canAffordGeodeCrackingRobot(state.ore, state.obsidian)) {
+                //AdventLogger.debug("Spend ${blueprint.geodeRobotCost} to start building a geode-cracking robot.")
+                /*decisionStates += CollectionState(
+                    ore = state.ore - blueprint.geodeRobotCost.first,
+                    oreCollectingRobots = state.oreCollectingRobots,
+                    clay = state.clay,
+                    clayCollectingRobots = state.clayCollectingRobots,
+                    obsidian = state.obsidian - blueprint.geodeRobotCost.second,
+                    obsidianCollectingRobots = state.obsidianCollectingRobots,
+                    openGeodes = state.openGeodes,
+                    geodeCrackingRobots = state.geodeCrackingRobots + 1
+                )*/
 
-            var openGeodes = 0
-            var geodeCrackingRobots = 0
-
-            while(minute <= 24) {
-                AdventLogger.debug("== Minute $minute ==")
-
-                val factory = mutableListOf<RobotType>()
-
-                // Resources are spent immediately on the factory when available
-                if (blueprint.canAffordGeodeCrackingRobot(ore, obsidian)) {
-                    AdventLogger.debug("Spend ${blueprint.geodeRobotCost} to start building a geode-cracking robot.")
-                    ore -= blueprint.geodeRobotCost.first
-                    obsidian -= blueprint.geodeRobotCost.second
-                    factory += RobotType.GEODE_ROBOT
-                }
-
-                if (blueprint.canAffordObsidianRobot(ore, clay)) {
-                    AdventLogger.debug("Spend ${blueprint.obsidianRobotCost} to start building an obsidian-collecting robot.")
-                    ore -= blueprint.obsidianRobotCost.first
-                    clay -= blueprint.obsidianRobotCost.second
-                    factory += RobotType.OBSIDIAN_ROBOT
-                }
-
-                if (ore >= blueprint.clayRobotCost) {
-                    AdventLogger.debug("Spend ${blueprint.clayRobotCost} to start building a clay-collecting robot.")
-                    ore -= blueprint.clayRobotCost
-                    factory += RobotType.CLAY_ROBOT
-                }
-
-                // Collect Ore
-                val oreCollected = oreCollectingRobots
-                ore += oreCollected
-                AdventLogger.debug("$oreCollectingRobots ore-collecting robot(s) collects $oreCollected ore; you now have $ore ore.")
-
-                // Collect Clay
-                val clayCollected = clayCollectingRobots
-                clay += clayCollected
-                if (clayCollected > 0) AdventLogger.debug("$clayCollectingRobots clay-collecting robot(s) collects $clayCollected clay; you now have $clay clay.")
-
-                // Collect Obsidian
-                val obsidianCollected = obsidianCollectingRobots
-                obsidian += obsidianCollected
-                if (obsidianCollected > 0) AdventLogger.debug("$obsidianCollectingRobots clay-collecting robot(s) collects $obsidianCollected obsidian; you now have $obsidian obsidian.")
-
-                // Crack Geodes
-                val geodesOpened = geodeCrackingRobots
-                openGeodes += geodesOpened
-                if (geodesOpened > 0) AdventLogger.debug("$geodeCrackingRobots geode-cracking robot(s) crack $geodesOpened geodes; you now have $openGeodes open geodes.")
-
-                // The minute is over, the factory finishes making whatever is in-progress
-                factory.forEach { robot -> when(robot) {
-                    RobotType.CLAY_ROBOT -> {
-                        clayCollectingRobots += 1
-                        AdventLogger.debug("The new clay-collecting robot is ready; you now have $clayCollectingRobots of them.")
-                    }
-                    RobotType.ORE_ROBOT -> {
-                        oreCollectingRobots += 1
-                        AdventLogger.debug("The new ore-collecting robot is ready; you now have $oreCollectingRobots of them.")
-                    }
-                    RobotType.OBSIDIAN_ROBOT -> {
-                        obsidianCollectingRobots += 1
-                        AdventLogger.debug("The new obsidian-collecting robot is ready; you now have $obsidianCollectingRobots of them.")
-                    }
-                    RobotType.GEODE_ROBOT -> {
-                        geodeCrackingRobots += 1
-                        AdventLogger.debug("The new geode-cracking robot is ready; you now have $geodeCrackingRobots of them.")
-                    }
-                }}
-
-                minute += 1
-
-                AdventLogger.debug("\n")
+                decisionStates += state.getGeodeCrackingRobotPurchaseState(blueprint.geodeRobotCost.first, blueprint.geodeRobotCost.second)
             }
 
-            blueprint.id to openGeodes
+            if (blueprint.canAffordObsidianRobot(state.ore, state.clay)) {
+                //AdventLogger.debug("Spend ${blueprint.obsidianRobotCost} to start building an obsidian-collecting robot.")
+                /*decisionStates += CollectionState(
+                    ore = state.ore - blueprint.obsidianRobotCost.first,
+                    oreCollectingRobots = state.oreCollectingRobots,
+                    clay = state.clay - blueprint.obsidianRobotCost.second,
+                    clayCollectingRobots = state.clayCollectingRobots,
+                    obsidian = state.obsidian,
+                    obsidianCollectingRobots = state.obsidianCollectingRobots + 1,
+                    openGeodes = state.openGeodes,
+                    geodeCrackingRobots = state.geodeCrackingRobots
+                )*/
+
+                decisionStates += state.getObsidianCollectingRobotPurchaseState(blueprint.obsidianRobotCost.first, blueprint.obsidianRobotCost.second)
+            }
+
+            if (state.ore >= blueprint.clayRobotCost) {
+                //AdventLogger.debug("Spend ${blueprint.clayRobotCost} to start building a clay-collecting robot.")
+                /*decisionStates += CollectionState(
+                    ore = state.ore - blueprint.clayRobotCost,
+                    oreCollectingRobots = state.oreCollectingRobots,
+                    clay = state.clay,
+                    clayCollectingRobots = state.clayCollectingRobots + 1,
+                    obsidian = state.obsidian,
+                    obsidianCollectingRobots = state.obsidianCollectingRobots,
+                    openGeodes = state.openGeodes,
+                    geodeCrackingRobots = state.geodeCrackingRobots
+                )*/
+                decisionStates += state.getClayCollectingRobotPurchaseState(blueprint.clayRobotCost)
+            }
+
+/*            // Collect Ore
+            val oreCollected = state.oreCollectingRobots
+            state.ore += oreCollected
+            //AdventLogger.debug("${state.oreCollectingRobots} ore-collecting robot(s) collects $oreCollected ore; you now have ${state.ore} ore.")
+
+            // Collect Clay
+            val clayCollected = state.clayCollectingRobots
+            state.clay += clayCollected
+            //if (clayCollected > 0) AdventLogger.debug("${state.clayCollectingRobots} clay-collecting robot(s) collects $clayCollected clay; you now have ${state.clay} clay.")
+
+            // Collect Obsidian
+            val obsidianCollected = state.obsidianCollectingRobots
+            state.obsidian += obsidianCollected
+            //if (obsidianCollected > 0) AdventLogger.debug("${state.obsidianCollectingRobots} clay-collecting robot(s) collects $obsidianCollected obsidian; you now have ${state.obsidian} obsidian.")
+
+            // Crack Geodes
+            val geodesOpened = state.geodeCrackingRobots
+            state.openGeodes += geodesOpened
+            //if (geodesOpened > 0) AdventLogger.debug("${state.geodeCrackingRobots} geode-cracking robot(s) crack $geodesOpened geodes; you now have ${state.openGeodes} open geodes.")
+
+            decisionStates.map { state ->
+                state.ore += oreCollected
+                state.clay += clayCollected
+                state.obsidian += obsidianCollected
+                state.openGeodes += geodesOpened
+            }*/
+
+            state.collect()
+
+            return (decisionStates + state).flatMap { outcome ->
+                cache.getOrPut("${blueprint.id}${state.key()}") { recurse(blueprint, minute + 1, outcome) }
+            }
+        } else {
+            val totalOpenGeodes = listOf(blueprint.id to state.openGeodes)
+            cache["${blueprint.id}${state.key()}"] = totalOpenGeodes
+            return totalOpenGeodes
+        }
+    }
+
+    inner class CollectionState(
+        var ore: Int,
+        private var oreCollectingRobots: Int,
+        var clay: Int,
+        private var clayCollectingRobots: Int,
+        var obsidian: Int,
+        private var obsidianCollectingRobots: Int,
+        var openGeodes: Int,
+        private var geodeCrackingRobots: Int
+    ) {
+        fun getGeodeCrackingRobotPurchaseState(oreCost: Int, obsidianCost: Int): CollectionState {
+            val state = CollectionState(
+                ore = ore - oreCost,
+                oreCollectingRobots = oreCollectingRobots,
+                clay = clay,
+                clayCollectingRobots = clayCollectingRobots,
+                obsidian = obsidian - obsidianCost,
+                obsidianCollectingRobots = obsidianCollectingRobots,
+                openGeodes = openGeodes,
+                geodeCrackingRobots = geodeCrackingRobots
+            )
+
+            state.collect()
+
+            state.geodeCrackingRobots += 1
+
+            return state
         }
 
-        return result.sumOf { (id, geodes) -> id * geodes }
+        fun getObsidianCollectingRobotPurchaseState(oreCost: Int, clayCost: Int): CollectionState {
+            val state = CollectionState(
+                ore = ore - oreCost,
+                oreCollectingRobots = oreCollectingRobots,
+                clay = clay - clayCost,
+                clayCollectingRobots = clayCollectingRobots,
+                obsidian = obsidian,
+                obsidianCollectingRobots = obsidianCollectingRobots,
+                openGeodes = openGeodes,
+                geodeCrackingRobots = geodeCrackingRobots
+            )
+
+            state.collect()
+
+            state.obsidianCollectingRobots += 1
+
+            return state
+        }
+
+        fun getClayCollectingRobotPurchaseState(oreCost: Int): CollectionState {
+            val state = CollectionState(
+                ore = ore - oreCost,
+                oreCollectingRobots = oreCollectingRobots,
+                clay = clay,
+                clayCollectingRobots = clayCollectingRobots,
+                obsidian = obsidian,
+                obsidianCollectingRobots = obsidianCollectingRobots,
+                openGeodes = openGeodes,
+                geodeCrackingRobots = geodeCrackingRobots
+            )
+
+            state.collect()
+
+            state.clayCollectingRobots += 1
+
+            return state
+        }
+
+        fun collect() {
+            ore += oreCollectingRobots
+            clay += clayCollectingRobots
+            obsidian += obsidianCollectingRobots
+            openGeodes += geodeCrackingRobots
+        }
+
+        fun key(): String {
+            return "$ore$oreCollectingRobots$clay$clayCollectingRobots$obsidian$obsidianCollectingRobots$openGeodes$geodeCrackingRobots"
+        }
     }
 
     enum class RobotType {
