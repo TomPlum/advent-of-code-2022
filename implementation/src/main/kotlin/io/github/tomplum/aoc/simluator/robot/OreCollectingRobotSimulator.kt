7aa24c9dd84414fc599d214c40987717a8ae02b2
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-19 21:47:45 +0000

    Day 19 | Finally got both blueprints passing

--- implementation/src/main/kotlin/io/github/tomplum/aoc/simluator/robot/OreCollectingRobotSimulator.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/simluator/robot/OreCollectingRobotSimulator.kt
@@ -1,12 +1,10 @@
 package io.github.tomplum.aoc.simluator.robot
 
-import io.github.tomplum.libs.extensions.nthBinomialCoefficient
-
 class OreCollectingRobotSimulator(data: List<String>) {
 
     private val blueprints = data.map { info -> Blueprint.fromString(info) }
 
-    private val cache = mutableMapOf<String, List<Int>>()
+    private val cache = mutableMapOf<String, Int>()
     private var maxGeodesFound = 0
 
     /**
@@ -15,60 +13,59 @@ class OreCollectingRobotSimulator(data: List<String>) {
     fun simulate(): Int {
         val start = InventoryState(0, 1, 0, 0, 0, 0, 0, 0)
         val geodes = blueprints.map { blueprint ->
-            blueprint.id to calculateGeodesSmashed(blueprint, 1, start).max()
+            blueprint.id to calculateGeodesSmashed(blueprint, 24, start)
         }
         return geodes.sumOf { (id, geodes) -> id * geodes }
     }
 
-    private fun calculateGeodesSmashed(blueprint: Blueprint, minute: Int, inventory: InventoryState): List<Int> {
-        if (minute <= 24) {
-            val states = mutableListOf<InventoryState>()
-
-        /*    val timeRemaining = 24 - minute
-            val optimisticGeodePrediction = (timeRemaining - 1).nthBinomialCoefficient()
-            if (optimisticGeodePrediction <= maxGeodesFound) {
-                return listOf(inventory.openGeodes)
-            }*/
-
-            if (inventory.canAffordGeodeCrackingRobot(blueprint)) {
-                states += inventory.createGeodeCrackingRobot(
-                    blueprint.geodeRobotCost.first,
-                    blueprint.geodeRobotCost.second
-                )
-            } else {
-                val hasEnoughObsidian = inventory.producesEnoughObsidianToMakeGeodeRobots(blueprint)
-                if (!hasEnoughObsidian && inventory.canAffordObsidianRobot(blueprint)) {
-                    states += inventory.createObsidianCollectingRobot(
-                        blueprint.obsidianRobotCost.first,
-                        blueprint.obsidianRobotCost.second
-                    )
-                }
-
-                val hasEnoughClay = inventory.producesEnoughClayToMakeObsidianRobots(blueprint)
-                if (!hasEnoughClay && inventory.canAffordClayRobot(blueprint)) {
-                    states += inventory.createClayCollectionRobot(blueprint.clayRobotCost)
-                }
-
-                val hasEnoughOre = inventory.producesEnoughOreToMakingEverything(blueprint)
-                if (!hasEnoughOre && inventory.canAffordOreRobot(blueprint)) {
-                    states += inventory.createOreCollectionRobot(blueprint.oreRobotCost)
-                }
-
-                if ((!hasEnoughOre || !hasEnoughClay || !hasEnoughObsidian) && states.isEmpty()) {
-                    states += inventory.createNothing()
-                }
-            }
-
-            return states.flatMap { nextState ->
-                cache.getOrPut("${blueprint.id}${nextState.key()}") {
-                    calculateGeodesSmashed(blueprint, minute + 1, nextState)
-                }
-            }
-        } else {
-            val geodes = inventory.openGeodes
-            if (geodes > maxGeodesFound) maxGeodesFound = geodes
-            return listOf(geodes)
+    private fun calculateGeodesSmashed(blueprint: Blueprint, minute: Int, inventory: InventoryState): Int {
+        if (minute == 0) {
+            maxGeodesFound = maxOf(maxGeodesFound, inventory.openGeodes)
+            return inventory.openGeodes
         }
+
+        if (inventory.canAffordGeodeCrackingRobot(blueprint)) {
+            val updatedInventory = inventory.createGeodeCrackingRobot(
+                blueprint.geodeRobotCost.first,
+                blueprint.geodeRobotCost.second
+            )
+            return searchDeeper(blueprint, minute, updatedInventory)
+        } else {
+            val hasEnoughObsidian = inventory.producesEnoughObsidianToMakeGeodeRobots(blueprint)
+            if (!hasEnoughObsidian && inventory.canAffordObsidianRobot(blueprint)) {
+                val updatedInventory = inventory.createObsidianCollectingRobot(
+                    blueprint.obsidianRobotCost.first,
+                    blueprint.obsidianRobotCost.second
+                )
+                maxGeodesFound = searchDeeper(blueprint, minute, updatedInventory)
+            }
+
+            val hasEnoughClay = inventory.producesEnoughClayToMakeObsidianRobots(blueprint)
+            if (!hasEnoughClay && inventory.canAffordClayRobot(blueprint)) {
+                val updatedInventory = inventory.createClayCollectionRobot(blueprint.clayRobotCost)
+                maxGeodesFound = searchDeeper(blueprint, minute, updatedInventory)
+            }
+
+            val hasEnoughOre = inventory.producesEnoughOreToMakingEverything(blueprint)
+            if (!hasEnoughOre && inventory.canAffordOreRobot(blueprint)) {
+                val updatedInventory = inventory.createOreCollectionRobot(blueprint.oreRobotCost)
+                maxGeodesFound = searchDeeper(blueprint, minute, updatedInventory)
+            }
+
+            if (!hasEnoughOre || !hasEnoughClay || !hasEnoughObsidian) {
+                val updatedInventory = inventory.createNothing()
+                maxGeodesFound = searchDeeper(blueprint, minute, updatedInventory)
+            }
+        }
+
+        return maxGeodesFound
+    }
+
+    private fun searchDeeper(blueprint: Blueprint, minute: Int, inventory: InventoryState): Int {
+        val geodes = cache.getOrPut("${blueprint.id}${inventory.key()}") {
+            calculateGeodesSmashed(blueprint, minute - 1, inventory)
+        }
+        return maxOf(maxGeodesFound, geodes)
     }
 
     data class InventoryState(
