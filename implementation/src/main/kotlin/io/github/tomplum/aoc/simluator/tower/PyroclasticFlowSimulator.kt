c731aa934c080cd425ccce138b11eaae9f31a02d
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-20 20:51:10 +0000

    Day 17 | Encapsulation improvements + method extraction

--- implementation/src/main/kotlin/io/github/tomplum/aoc/simluator/tower/PyroclasticFlowSimulator.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/simluator/tower/PyroclasticFlowSimulator.kt
@@ -6,67 +6,40 @@ class PyroclasticFlowSimulator(data: String) {
 
     private val flow = PyroclasticFlow(data)
 
+    private var count = 0
+    private var rocks = 0L
+
+    private var x = 3
+    private var y = 4
+    private var extrapolatedHeight = 0L
+
+    private var currentRock = flow.getNextRock()
+
     fun simulate(quantity: Long): Long {
-        var count = 0
-        var rocks = 0L
 
-        var x = 3
-        var y = 4
 
-        var currentRock = flow.getNextRock()
-
-        var extrapolatedHeight = 0L
         val states = mutableMapOf<StateKey, StateValue>()
 
         while(rocks < quantity) {
 
             if (rocks > 1000) {
-                val stateKey = StateKey(flow.rockIndex, flow.jetIndex)
+                val key = StateKey(flow.rockIndex, flow.jetIndex)
 
-                if (stateKey in states) {
-                    val (previousRocks, previousHeight) = states[stateKey]!!
-                    val period = rocks - previousRocks
-                    if (period > 0 && rocks % period == quantity % period) {
-                        val cycleHeight = flow.highestRockPosition - previousHeight
-                        val remainingFullCycles = (quantity - rocks) / period
-                        val extrapolatedRocks = remainingFullCycles * period
-                        extrapolatedHeight = cycleHeight * remainingFullCycles
-                        rocks += extrapolatedRocks
-                    }
+                if (key in states) {
+                    val (extrapolatedRocks, extrapolatedHeight) = states[key]!!.extrapolateFlowHeight(quantity)
+                    rocks += extrapolatedRocks
+                    this.extrapolatedHeight = extrapolatedHeight
                 } else {
-                    states[stateKey] = StateValue(rocks.toInt(), flow.highestRockPosition)
+                    states[key] = StateValue(rocks.toInt(), flow.highestRockPosition)
                 }
             }
 
             val isInfluencedByJetStream = count % 2 == 0
+
             if (isInfluencedByJetStream) {
-                val direction = flow.getNextJetPatternDirection()
-                val xLeftNew = Point2D(x, y).shift(direction).x
-                val newRockPositions = currentRock.positions(Point2D(xLeftNew, y))
-
-                val xBoundary = (1..7)
-                val xRightNew = currentRock.xRightMost(xLeftNew)
-
-                val isWithinBounds = xLeftNew in xBoundary && xRightNew in xBoundary
-                val isShiftingIntoRock = flow.hasAnyRocksResting(newRockPositions)
-
-                if (isWithinBounds && !isShiftingIntoRock) {
-                    x = xLeftNew
-                }
+                pushRockViaJetStream()
             } else {
-                val yNew = y - 1
-                val newRockPositions = currentRock.positions(Point2D(x, yNew)).reversed()
-
-                val rockWillHitRestingPoint = flow.hasAnyRocksResting(newRockPositions) || newRockPositions.any { pos -> pos.y == 0 }
-                if (rockWillHitRestingPoint) {
-                    flow.addRestingRock(currentRock.positions(Point2D(x, y)))
-                    currentRock = flow.getNextRock()
-                    x = 3
-                    y = flow.highestRockPosition + currentRock.height() + 3
-                    rocks += 1
-                } else {
-                    y = yNew
-                }
+                letRockFall()
             }
 
             count++
@@ -75,7 +48,53 @@ class PyroclasticFlowSimulator(data: String) {
         return flow.highestRockPosition.toLong() + extrapolatedHeight
     }
 
+    private fun pushRockViaJetStream() {
+        val direction = flow.getNextJetPatternDirection()
+        val xLeftNew = Point2D(x, y).shift(direction).x
+        val newRockPositions = currentRock.positions(Point2D(xLeftNew, y))
+
+        val xBoundary = (1..7)
+        val xRightNew = currentRock.xRightMost(xLeftNew)
+
+        val isWithinBounds = xLeftNew in xBoundary && xRightNew in xBoundary
+        val isShiftingIntoRock = flow.hasAnyRocksResting(newRockPositions)
+
+        if (isWithinBounds && !isShiftingIntoRock) {
+            x = xLeftNew
+        }
+    }
+
+    private fun letRockFall(): Rock {
+        val yNew = y - 1
+        val newRockPositions = currentRock.positions(Point2D(x, yNew)).reversed()
+
+        val isComingToRest = flow.hasAnyRocksResting(newRockPositions) || newRockPositions.any { pos -> pos.y == 0 }
+        if (isComingToRest) {
+            flow.addRestingRock(currentRock.positions(Point2D(x, y)))
+            currentRock = flow.getNextRock()
+            x = 3
+            y = flow.highestRockPosition + currentRock.height() + 3
+            rocks += 1
+        } else {
+            y = yNew
+        }
+        return currentRock
+    }
+
     data class StateKey(val rockTypeIndex: Int, val jetStreamIndex: Int)
 
-    data class StateValue(val previousRockCount: Int, val previousFlowHeight: Int)
+    inner class StateValue(private val previousRockCount: Int, private val previousFlowHeight: Int) {
+        fun extrapolateFlowHeight(rockQuantity: Long): Pair<Long, Long> {
+            val period = rocks - previousRockCount
+            if (period > 0 && rocks % period == rockQuantity % period) {
+                val cycleHeight = flow.highestRockPosition - previousFlowHeight
+                val remainingFullCycles = (rockQuantity - rocks) / period
+                val extrapolatedRocks = remainingFullCycles * period
+                val extrapolatedHeight = cycleHeight * remainingFullCycles
+                return extrapolatedRocks to extrapolatedHeight
+            }
+
+            return 0L to 0L
+        }
+    }
 }
