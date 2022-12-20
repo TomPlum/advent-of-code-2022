7fbf786b699b83ca6f9ecc281d0f72a8c8587ee5
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-20 08:57:45 +0000

    Day 20 | Added part one solution

---
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day20.kt
@@ -0,0 +1,15 @@
+package io.github.tomplum.aoc.solutions
+
+import io.github.tomplum.aoc.communication.encryption.EncryptedFile
+import io.github.tomplum.libs.input.Day
+import io.github.tomplum.libs.input.InputReader
+import io.github.tomplum.libs.solutions.Solution
+
+class Day20 : Solution<Int, Int> {
+    private val contents = InputReader.read<Int>(Day(20)).value
+    private val file = EncryptedFile(contents)
+
+    override fun part1(): Int {
+        return file.mix()
+    }
+}
