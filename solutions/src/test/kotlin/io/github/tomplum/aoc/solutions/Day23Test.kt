58d260ffe3577e938b947e62ca40a39e514802d2
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-23 17:09:15 +0000

    Day 23 | Added solution part two

--- solutions/src/test/kotlin/io/github/tomplum/aoc/solutions/Day23Test.kt
+++ solutions/src/test/kotlin/io/github/tomplum/aoc/solutions/Day23Test.kt
@@ -11,4 +11,9 @@ class Day23Test {
     fun partOne() {
         assertThat(solution.part1()).isEqualTo(4302)
     }
+
+    @Test
+    fun partTwo() {
+        assertThat(solution.part2()).isEqualTo(1025)
+    }
 }
