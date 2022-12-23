9c4505996af35ba65bbc2b9b36ccf59d9f181bac
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-23 17:05:03 +0000

    Day 23 | Added solution part one

---
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day23.kt
@@ -0,0 +1,15 @@
+package io.github.tomplum.aoc.solutions
+
+import io.github.tomplum.aoc.simluator.tree.StarFruitSimulator
+import io.github.tomplum.libs.input.Day
+import io.github.tomplum.libs.input.InputReader
+import io.github.tomplum.libs.solutions.Solution
+
+class Day23 : Solution<Int, Int> {
+    private val notes = InputReader.read<String>(Day(23)).value
+    private val simulator = StarFruitSimulator(notes)
+
+    override fun part1(): Int {
+        return simulator.findElvenBoundingRectangle()
+    }
+}
