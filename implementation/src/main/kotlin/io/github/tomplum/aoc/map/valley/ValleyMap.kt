8b314adeaeaa902282ccee589de138281c93804e
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-24 14:21:29 +0000

    Day 24 | Add part one solution

--- implementation/src/main/kotlin/io/github/tomplum/aoc/map/valley/ValleyMap.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/map/valley/ValleyMap.kt
@@ -1,11 +1,18 @@
 package io.github.tomplum.aoc.map.valley
 
+import io.github.tomplum.extension.lcm
 import io.github.tomplum.libs.logging.AdventLogger
 import io.github.tomplum.libs.math.Direction
 import io.github.tomplum.libs.math.map.AdventMap2D
 import io.github.tomplum.libs.math.point.Point2D
 
 class ValleyMap(data: List<String>) : AdventMap2D<ValleyTile>() {
+
+    private var xMin: Int
+    private var xMax: Int
+    private var yMin: Int
+    private var yMax: Int
+
     init {
         var x = 0
         var y = 0
@@ -19,90 +26,107 @@ class ValleyMap(data: List<String>) : AdventMap2D<ValleyTile>() {
             x = 0
             y++
         }
+
+        xMin = xMin()!!
+        xMax = xMax()!!
+        yMin = yMin()!!
+        yMax = yMax()!!
     }
 
     fun traverseBlizzards(): Int {
-        val start = filterTiles { tile -> tile.isClear() }.minBy { (pos, _) -> pos.y }.key
-        val goal = filterTiles { tile -> tile.isClear() }.maxBy { (pos, _) -> pos.y }.key
-
-        var steps = 0
-
-        val next = mutableSetOf<Point2D>()
-        next.add(start)
-
-        var lastPositions: MutableSet<Point2D>
-        val visited = mutableSetOf<Point2D>()
-
-        while(next.isNotEmpty() && !next.contains(goal)) {
-            // Move Blizzards
+        val blizzardTimeLcm = 600//listOf(xMax - 2L, yMax - 2L).lcm().toInt()
+        val initialState = filterTiles { it.isClear() }.keys
+        AdventLogger.debug("Intitial State \n$this\n")
+        val seen = mutableListOf(initialState)
+        val clearTiles = (1..blizzardTimeLcm).associateWith {
+            // Calculate new blizzard positions
             val currentBlizzardPositions = filterTiles { tile -> tile.hasBlizzard() }
             val newBlizzardPositions = currentBlizzardPositions.flatMap { (pos, tile) ->
                 tile.getBlizzardDirections().map { (direction, symbol) ->
                     val newPosition = pos.shift(direction)
                     if (getTile(newPosition, ValleyTile(listOf('.'))).isWall()) {
-                        newPosition.getNewBlizzardPosition(direction) to symbol
+                        newPosition.getBlizzardSpawnPosition(direction) to symbol
                     } else {
                         newPosition to symbol
                     }
                 }
             }
 
+            // Clear existing blizzards
             filterTiles { tile -> tile.hasBlizzard() }.keys.forEach { pos -> addTile(pos, ValleyTile(listOf('.'))) }
+
+            // Move Blizzards
             newBlizzardPositions.groupBy { it.first }.forEach { (newPosition, blizzards) ->
                 addTile(newPosition, ValleyTile(blizzards.map { it.second }))
             }
 
+            if (filterTiles { it.isClear() }.keys in seen) {
+                AdventLogger.debug("Found $it")
+            }
+
+            seen.add(filterTiles { it.isClear() }.keys)
+            //AdventLogger.debug("Minute $it\n$this\n")
+
+            filterTiles { tile -> tile.isClear() }.keys
+        }.toMutableMap()
+        clearTiles[0] = initialState
+        val start = filterTiles { tile -> tile.isClear() }.minBy { (pos, _) -> pos.y }.key
+        val goal = filterTiles { tile -> tile.isClear() }.maxBy { (pos, _) -> pos.y }.key
+        //addTile(Point2D(start.x, start.y - 1), ValleyTile(listOf('#')))
+       // addTile(Point2D(goal.x, goal.y + 1), ValleyTile(listOf('#')))
+
+        var minutesElapsed = 0
+        var clearIndex = 0
+
+        var currentPositions = mutableSetOf<Point2D>()
+        currentPositions.add(start)
+
+        while(true) {
+            if (goal in currentPositions) {
+                return minutesElapsed - 1
+            }
+
+            if (clearIndex > blizzardTimeLcm) {
+                clearIndex = 0
+            }
+
             // Move Person
-            val adjacent = filterPoints(next.flatMap { pos -> pos.orthogonallyAdjacent() }.toSet())
-            val traversable = adjacent.filterValues { tile -> tile.isClear() }.keys.filter { it !in visited }
-            lastPositions = next.toMutableSet()
-            visited.addAll(next)
-            next.clear()
+            val newPositionCandidates = mutableSetOf<Point2D>()
+            val clearAfterBlizzardsMoved = clearTiles[clearIndex]!!
+            currentPositions.forEach { position ->
+                val adjacentPositions = position.orthogonallyAdjacent().filter { pos -> pos in clearAfterBlizzardsMoved }
+                newPositionCandidates.addAll(adjacentPositions)
+                if (position in clearAfterBlizzardsMoved) {
+                    newPositionCandidates.add(position)
+                }
+            }
 
-            next.addAll(traversable)
-            next.addAll(lastPositions)
+            currentPositions = newPositionCandidates
+            //val nextPositions = adjacentPositions.filter { pos -> pos in clearAfterBlizzardsMoved }
+            //newPositionCandidates.addAll(nextPositions)
+
+            // Can we wait in any of the current?
+            //currentPositions.filter {  }
 
 
-            steps += 1
-            //AdventLogger.debug("Minute $steps\n$this\n")
+            //val allCandidates =  currentPositions.toMutableSet() + adjacentPositions
+
+            //val lastPositions =.filter { pos -> pos in clearAfterBlizzardsMoved }
+
+            //currentPositions.addAll(nextPositions)
+            //currentPositions.addAll(lastPositions)
+
+            minutesElapsed += 1
+            clearIndex += 1
+            //AdventLogger.debug("Minute $minutesElapsed\n$this\n")
         }
-
-        return steps + 1
     }
 
-    private fun Char.toDirection() = when(this) {
-        '>' -> Direction.RIGHT
-        '<' -> Direction.LEFT
-        'v' -> Direction.UP
-        '^' -> Direction.DOWN
-        else -> throw IllegalArgumentException("Tile does not contain any blizzards")
-    }
-
-    private fun Point2D.getNewBlizzardPosition(direction: Direction): Point2D {
-        if (direction == Direction.RIGHT) {
-            return filterTiles { tile -> tile.isClear() || tile.hasBlizzard() }.keys
-                .filter { pos -> pos.y == this.y }
-                .minBy { pos -> pos.x }
-        }
-
-        if (direction == Direction.LEFT) {
-            return filterTiles { tile -> tile.isClear() || tile.hasBlizzard() }.keys
-                .filter { pos -> pos.y == this.y }
-                .maxBy { pos -> pos.x }
-        }
-
-        if (direction == Direction.UP) {
-            return filterTiles { tile -> tile.isClear() || tile.hasBlizzard() }.keys
-                .filter { pos -> pos.x == this.x }
-                .minBy { pos -> pos.y }
-        }
-
-        if (direction == Direction.DOWN) {
-            return filterTiles { tile -> tile.isClear() || tile.hasBlizzard() }.keys
-                .filter { pos -> pos.x == this.x }
-                .maxBy { pos -> pos.y }
-        }
-
-        throw IllegalArgumentException("A blizzard cannot be facing [$direction]")
+    private fun Point2D.getBlizzardSpawnPosition(direction: Direction) = when(direction) {
+        Direction.RIGHT -> Point2D(xMin + 1, this.y)
+        Direction.LEFT -> Point2D(xMax - 1, this.y)
+        Direction.UP -> Point2D(this.x, yMin + 1)
+        Direction.DOWN -> Point2D(this.x, yMax - 1)
+        else -> throw IllegalArgumentException("Blizzards cannot move in [$direction]")
     }
 }
---
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day24.kt
@@ -0,0 +1,15 @@
+package io.github.tomplum.aoc.solutions
+
+import io.github.tomplum.aoc.map.valley.ValleyMap
+import io.github.tomplum.libs.input.Day
+import io.github.tomplum.libs.input.InputReader
+import io.github.tomplum.libs.solutions.Solution
+
+class Day24 : Solution<Int, Int> {
+    private val data = InputReader.read<String>(Day(24)).value
+    private val map = ValleyMap(data)
+
+    override fun part1(): Int {
+        return map.traverseBlizzards()
+    }
+}
