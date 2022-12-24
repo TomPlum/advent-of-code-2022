e01391a9aa68f7dcde7765e0a5b73806978aacdc
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2022-12-24 23:18:20 +0000

    Day 22 | Added solution part two

--- solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day22.kt
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day22.kt
@@ -1,15 +1,21 @@
 package io.github.tomplum.aoc.solutions
 
 import io.github.tomplum.aoc.map.monkey.MonkeyMap
+import io.github.tomplum.aoc.map.monkey.MonkeyMap3D
 import io.github.tomplum.libs.input.Day
 import io.github.tomplum.libs.input.InputReader
 import io.github.tomplum.libs.solutions.Solution
 
 class Day22 : Solution<Int, Int> {
     private val notes = InputReader.read<String>(Day(22)).value
-    private val map = MonkeyMap(notes)
 
     override fun part1(): Int {
+        val map = MonkeyMap(notes)
+        return map.findFinalPassword()
+    }
+
+    override fun part2(): Int {
+        val map = MonkeyMap3D(notes)
         return map.findFinalPassword()
     }
 }
